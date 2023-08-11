f6ddeb4dd055dacde2cf63aced91de11ffe172c9
theshaikhroman <shaikhroman8@gmail.com>
2023-08-11 15:20:05 +0530

    Initial commit : Example.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Example } from './components/Example';
 
 function App() {
   return (
@@ -18,7 +19,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <Example/>
     </div>
+    
+    
   );
 }
 
---
+++ src/components/Example.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Example =()=> {
+  return (
+    <div>
+      <h1>Hey Everyone </h1>
+    </div>
+  )
+}
